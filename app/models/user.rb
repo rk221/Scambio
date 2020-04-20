a67f7b3afd5ed50a421996dadd3be1ade488b90f
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-04-20 12:11:27 +0900

    Add validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 名前は全角平仮名、全角カタカナ、漢字（鬼車）のみ許可
+  VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  validates :firstname, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :lastname, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :nickname, presence: true, length: {in: 4..30}
 end
