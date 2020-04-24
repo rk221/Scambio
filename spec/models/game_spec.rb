994d37e964eda2b75f5648e8f4a9620a05a9ba8b
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-04-24 17:28:06 +0900

    Add ModelTest to Game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "タイトルがある場合、有効である" do
+    game = FactoryBot.build(:game)
+    expect(game).to be_valid
+  end
+
+  it "タイトルがない場合、無効である" do
+    game = FactoryBot.build(:game, title: nil)
+    game.valid?
+    expect(game.errors[:title]).to include("を入力してください")
+  end
 end
