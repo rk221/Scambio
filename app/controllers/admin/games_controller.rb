6b2f84ea25f273502a74fb0bfa9be758013946e2
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-04-25 21:47:29 +0900

    Add new game registration function

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -2,4 +2,24 @@ class Admin::GamesController < AdminController
     def index 
         @games = Game.all
     end
+
+    def new
+        @game = Game.new
+    end
+    
+    def create 
+        @game = Game.new(game_params)
+        if @game.save
+            redirect_to admin_games_path, notice: 'ゲームを新規登録しました'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:id, :title, :image_icon)
+    end
+    
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
     validates :title, presence: true
+    
+    mount_uploader :image_icon, ImageUploader
 end
