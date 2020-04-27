cfd15f386c132828884ffb1e077c59c67702543b
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-04-27 14:53:49 +0900

    Add breadcrumb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,30 @@ crumb :root do
   link "Home", root_path
 end
 
+crumb :profile do 
+  link "プロフィール", users_show_path
+end
+
+crumb :edit_profile do
+  link "プロフィール変更", edit_user_registration_path
+  parent :profile
+end
+
+crumb :games do 
+  link "ゲーム一覧", admin_games_path
+end
+
+crumb :new_game do 
+  link "ゲーム登録", new_admin_game_path
+  parent :games
+end
+
+crumb :edit_game do 
+  link "ゲーム編集", edit_admin_game_path
+  parent :games
+end
+
+
 # crumb :projects do
 #   link "Projects", projects_path
 # end
