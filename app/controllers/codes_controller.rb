0276eae795470780c6e82abdc6ebfd3fd741dc3d
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-04-30 17:10:00 +0900

    Add psn_id index to codes

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -2,5 +2,7 @@ class CodesController < ApplicationController
     def index
        @nintendo_friend_code = current_user.nintendo_friend_code
        @nintendo_friend_code = Codes::NintendoFriendCodeDecorator.new(@nintendo_friend_code) if @nintendo_friend_code.present?
+       
+       @play_station_network_id = current_user.play_station_network_id
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :codes, only: [:index]
   namespace :codes do
     resources :nintendo_friend_codes, only: [:new, :create, :edit, :update, :destroy]
+    resources :play_station_network_ids, only: [:new, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#show'
