8f084ec8b7213ec88efb4b43a69bfffbcc26c428
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-05-03 12:41:30 +0900

    Add item genre index to view

--- app/controllers/admin/item_genres_controller.rb
+++ app/controllers/admin/item_genres_controller.rb
@@ -1,2 +1,5 @@
 class Admin::ItemGenresController < AdminController
+    def index
+        @item_genres = ItemGenre.all
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :games
+    resources :item_genres
   end
 
   resources :codes, only: [:index]
