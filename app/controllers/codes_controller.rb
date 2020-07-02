1a26cd017f9610496e07ea6be2549287142ec371
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-07-02 15:07:25 +0900

    Fix code
    ぼっち演算子を、適用し直し。条件分岐を整理

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,7 +1,6 @@
 class CodesController < ApplicationController
     def index
-       @nintendo_friend_code = current_user.nintendo_friend_code
-       @nintendo_friend_code = @nintendo_friend_code.decorate if @nintendo_friend_code.present?
+       @nintendo_friend_code = current_user.nintendo_friend_code&.decorate
        
        @play_station_network_id = current_user.play_station_network_id
     end
