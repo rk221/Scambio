32fe4f1adfb767179e852844552b2abb5a184374
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-07-04 23:10:56 +0900

    Fix item_trade_queue

--- app/models/item_trade_queue.rb
+++ app/models/item_trade_queue.rb
@@ -10,11 +10,30 @@ class ItemTradeQueue < ApplicationRecord
 
     # キューが購入されていて、かつ、有効（取引中）の状態
     scope :exist_user_enabled, -> {where.not(item_trade_queues: {user_id: nil}).where(item_trade_queues: {enable_flag: true})}
-    # 反応待ち取引一覧
+    # 売却中の取引一覧（ユーザが存在する）
     scope :reaction_wait_item_trade_queues, -> (current_user_id) do 
         exist_user_enabled
         .includes({item_trade: [:game, {buy_item: :item_genre}, {sale_item: :item_genre}, :user_game_rank]}, :item_trade_detail)
         .where(item_trades: {user_id: current_user_id}, item_trade_details: {buy_popuarity: nil})
+    end
+    # 購入中の取引一覧
+    scope :trade_under_purchases, -> (current_user_id) do
+        exist_user_enabled
+        .includes({item_trade: [:user, :game, {buy_item: :item_genre}, {sale_item: :item_genre}, :user_game_rank]}, :item_trade_detail)
+        .where(item_trade_queues: {user_id: current_user_id}, item_trade_details: {sale_popuarity: nil})
+    end
+
+    def buy(current_user_id)
+        self.transaction do
+            update!(user_id: current_user_id)
+            # 成立メッセージを相手に送信
+            UserMessagePost.create_message_sell!(self)
+            # ゲームランクを生成する user_idとgame_idで一意でなければvalidationで弾く。
+            UserGameRank.create(user_id: current_user_id, game_id: self.item_trade.game_id)
+        end
+        true
+        rescue
+        false
     end
 
     # アイテムトレードに対応する購入待機用枠を作成する
