10d6a2cab1fb4924a78bbab66a9562250452a72f
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-07-17 09:46:48 +0900

    Fix pagenation

--- app/models/concerns/pagenation.rb
+++ app/models/concerns/pagenation.rb
@@ -1,20 +1,20 @@
 module Pagenation
     extend ActiveSupport::Concern
   
-    class ActiveRecord::Relation
-        attr_reader :now_page, :total_count, :total_page
+    included do
+        NUMBER_OF_OUTPUT_LINES = 1 # １ページの件数
 
-        NUMBER_OF_OUTPUT_LINES = 10 # １ページの件数
-
-        def page(page_params = '1')
-            @now_page = page_to_i(page_params)
+        scope :page, -> (page_params = '1') do 
+            @now_page = self.page_to_i(page_params)
             @total_count = all.size
             @total_page = (@total_count.to_f / NUMBER_OF_OUTPUT_LINES.to_f).ceil # 割った結果を切り上げし、1から始まるページ数を適切に算出
-            all.limit(NUMBER_OF_OUTPUT_LINES).offset((@now_page - 1) * NUMBER_OF_OUTPUT_LINES) # 自分自身のオブジェクトを、表示する部分だけ抽出したものを返す
+            all.limit(NUMBER_OF_OUTPUT_LINES).offset((@now_page - 1) * NUMBER_OF_OUTPUT_LINES).extending do # 自分自身のオブジェクトを、表示する部分だけ抽出したものを返す
+                attr_reader :now_page, :total_count, :total_page
+            end
         end
+    end
 
-        private
-
+    class_methods do
         def page_to_i(page_params)#整数の場合 数値変換し、返す 違う場合 1 を返す
             /\A[0-9]+\z/.match?(page_params) ? page_params.to_i : 1
         end
