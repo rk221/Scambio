db01bf80805fddf326925123e9fbf14dc03328ef
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-08-01 22:54:25 +0900

    Add show item trade queue size to mypage
    マイページにアイテムトレード購入待ち件数を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def show
     @user_game_ranks = current_user.user_game_ranks.order(:updated_at).limit(3).includes(:game).decorate
     @reaction_wait_item_trade_queues = ItemTradeQueue.reaction_wait_item_trade_queues(current_user.id).decorate
+    @item_trade_queue_size = ItemTradeQueue.trade_under_purchases(current_user.id).size
   end
 
   private
