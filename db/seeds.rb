ceab6f8b62dab968fbf4a41e9377b1fc6e6811b7
rk221 <kd1242488@st.kobedenshi.ac.jp>
2020-08-29 22:17:32 +0900

    Add seed to game image-icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require "csv"
+
+# 画像の存在確認をし、存在すればパスを返す
+def image_exist?(image_name, table_name: nil, extension: ".jpg")
+    table_name ||= @table_name
+    
+    path = Rails.root.join("db/seeds/", @environment, "images", table_name, image_name + extension)
+    if File.exist?(path)
+        path
+    else 
+        false
+    end
+end
+
 # データを投入するテーブル名を指定(seeds/以下のファイル名, 作成順に気を付ける)
 table_names = %w(users games badges item_genres)
 
 # ファイルの読み込み
 table_names.each do |table_name|
-    environment = (Rails.env == "test") ? "development" : Rails.env
-    
-	path = Rails.root.join("db/seeds", environment, table_name + ".rb")
+    @environment = (Rails.env == "test") ? "development" : Rails.env
+    @table_name = table_name
+	path = Rails.root.join("db/seeds", @environment, @table_name + ".rb")
     if File.exist?(path)
         puts "#{table_name}..."
         require path
--- db/seeds/development/games.rb
+++ db/seeds/development/games.rb
@@ -1,6 +1,9 @@
 CSV.foreach('db/seeds/development/csv/games.csv', headers: true) do |row|
+
     game = Game.new(title: row['title'])
-    unless game.save 
+    path = image_exist?(row['title'])
+    game.image_icon = open(path) if path
+    unless game.save
         p "game_save_error: #{game.errors}"
     end
 end
